272b0810ba74af86438f41595e03855d6c493ae1
febrimipu <singalada@gmail.com>
2024-05-11 19:06:53 +0700

    Setup Seeder Permission

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Create the initial roles and permissions.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'Super-Admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+
+        $role2 = Role::create(['name' => 'Admin']);
+        $role2->givePermissionTo('edit articles');
+        $role2->givePermissionTo('delete articles');
+        $role2->givePermissionTo('publish articles');
+        $role2->givePermissionTo('unpublish articles');
+
+        $role3 = Role::create(['name' => 'Bendahara']);
+        $role4 = Role::create(['name' => 'Guru']);
+        $role5 = Role::create(['name' => 'Staff']);
+        $role6 = Role::create(['name' => 'PPDB']);
+        $role7 = Role::create(['name' => 'Murid']);
+        $role8 = Role::create(['name' => 'Guest']);
+        $role9 = Role::create(['name' => 'Orang-Tua']);
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Super-Admin Userr',
+            'email' => 'superadmin@example.com',
+        ]);
+        $user->assignRole($role1);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+        ]);
+        $user->assignRole($role2);
+    }
+}
